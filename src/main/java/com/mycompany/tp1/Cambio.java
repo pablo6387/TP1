f3f1edc2c7bfc397f92479e5cd89078a2db5a672
pablo6387 <56855366+pablo6387@users.noreply.github.com>
2023-08-19 16:28:22 -0300

    Agregar clase cambio

---
+++ src/main/java/com/mycompany/tp1/Cambio.java
@@ -0,0 +1,18 @@
+package com.mycompany.tp1;
+
+public class Cambio {
+    private int version;
+
+    public Cambio(int version) {
+        this.version = version;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+    
+}
